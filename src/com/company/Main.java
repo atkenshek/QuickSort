a24b1ccf57092232731dd5a5e17974fbf006e481
Professional <random9804@yandex.ru>
2021-05-14 23:48:24 +0600

    Added comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,26 +3,27 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public void quickSort(int[] x){
+    public static void quickSort(int[] x){
         quickSort(x, 0, x.length - 1);
     }
-    public static void swap(int[] x, int i, int j){
+    public static void swap(int[] x, int i, int j){ //Created swap method
         int temp = x[i];
         x[i] = x[j];
         x[j] = temp;
     }
     private static void quickSort(int[] x, int start, int end){
         if(start < end){
-            int pi = partition(x, start, end);
-            quickSort(x, start, pi - 1); //Before pi
-            quickSort(x, pi + 1, end); //After pi
+            int pi = partition(x, start, end); //pi is partitioning index
+            quickSort(x, start, pi - 1); //Sort left half
+            quickSort(x, pi + 1, end); //Sort right half
         }
     }
     private static int partition(int[] x, int start, int end){
-        int pivot = x[end];
-        int i = start - 1;
+        int pivot = x[end]; //Select the pivot point
+        int i = start - 1; //Index of right position of pivot found
         for(int j = start; j <= end - 1; j++){
-            if(x[j] < pivot){
+            if(x[j] < pivot){ //if element smaller than pivot
+                //Increment index of smaller element and do swap
                 i++;
                 swap(x, i, j);
             }
@@ -32,15 +33,15 @@ public class Main {
 
     }
     public static void main(String[] args) {
-        int n;
+        int n; //Size of array
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
         int[] x = new int[n];
         for(int i = 0; i < n; i++){
-            x[i] = scanner.nextInt();
+            x[i] = scanner.nextInt(); //Input array
         }
-        quickSort(x, 0, n-1);
-        for(int i = 0; i < n; i++){
+        quickSort(x); //Called our method
+        for(int i = 0; i < n; i++){ //Output the result
             System.out.print(x[i] + " ");
         }
     }
